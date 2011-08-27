65860903d7136bafbc8b951caada97555e7c2661
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2011-08-27 16:33:01 +0200

    added Must

---
+++ src/Testing.Commons.Tests/MustTester.cs
@@ -0,0 +1,72 @@
+﻿using Dgg.Testing;
+using NUnit.Framework;
+
+namespace Testing.Commons.Tests
+{
+	[TestFixture]
+	public class MustTester
+	{
+		[Test]
+		public void Must_Be_CanBeExtended_ByAddingExtensionsMethodsToEntryPoint()
+		{
+			Assert.That(() => Must.Be.Extended(), Throws.Nothing);
+			Assert.That(() => Must.Be.Extended(3), Throws.Nothing);
+		}
+
+		[Test]
+		public void Must_Have_CanBeExtended_ByAddingExtensionsMethodsToEntryPoint()
+		{
+			Assert.That(() => Must.Have.Extended(), Throws.Nothing);
+			Assert.That(() => Must.Have.Extended(3), Throws.Nothing);
+		}
+
+		[Test]
+		public void Must_Match_CanBeExtended_ByAddingExtensionsMethodsToEntryPoint()
+		{
+			Assert.That(() => Must.Match.Extended(), Throws.Nothing);
+			Assert.That(() => Must.Match.Extended(3), Throws.Nothing);
+		}
+
+		[Test]
+		public void Must_NotBe_CanBeExtended_ByAddingExtensionsMethodsToEntryPoint()
+		{
+			Assert.That(() => Must.Not.Be.Extended(), Throws.Nothing);
+			Assert.That(() => Must.Not.Be.Extended(3), Throws.Nothing);
+		}
+
+		[Test]
+		public void Must_NotHave_CanBeExtended_ByAddingExtensionsMethodsToEntryPoint()
+		{
+			Assert.That(() => Must.Not.Have.Extended(), Throws.Nothing);
+			Assert.That(() => Must.Not.Have.Extended(3), Throws.Nothing);
+		}
+
+		[Test]
+		public void Must_NotMatch_CanBeExtended_ByAddingExtensionsMethodsToEntryPoint()
+		{
+			Assert.That(() => Must.Not.Match.Extended(), Throws.Nothing);
+			Assert.That(() => Must.Not.Match.Extended(3), Throws.Nothing);
+		}
+	}
+
+	internal static class MustExtensions
+	{
+		public static void Extended(this Must.BeEntryPoint entry) { }
+		public static void Extended(this Must.BeEntryPoint entry, int argument) { }
+
+		public static void Extended(this Must.HaveEntryPoint entry) { }
+		public static void Extended(this Must.HaveEntryPoint entry, int argument) { }
+
+		public static void Extended(this Must.MatchEntryPoint entry) { }
+		public static void Extended(this Must.MatchEntryPoint entry, int argument) { }
+
+		public static void Extended(this Must.NotBeEntryPoint entry) { }
+		public static void Extended(this Must.NotBeEntryPoint entry, int argument) { }
+
+		public static void Extended(this Must.NotHaveEntryPoint entry) { }
+		public static void Extended(this Must.NotHaveEntryPoint entry, int argument) { }
+
+		public static void Extended(this Must.NotMatchEntryPoint entry) { }
+		public static void Extended(this Must.NotMatchEntryPoint entry, int argument) { }
+	}
+}
---
+++ src/Testing.Commons/Enumerable.Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Dgg.Testing
+{
+	public static class EnumerableExtensions
+	{
+		public static IEnumerable<T> Iterate<T>(this IEnumerable<T> enumerable)
+		{
+			foreach (var v in enumerable) { yield return v; }
+		}
+	}
+}
