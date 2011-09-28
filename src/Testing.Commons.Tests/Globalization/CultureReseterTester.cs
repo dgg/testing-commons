ecfe73733b8a8a9aea40b97b310cca704302a7ca
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2011-09-28 21:29:17 +0200

    added CultureReseter

---
+++ src/Testing.Commons.Tests/Globalization/CultureReseterTester.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using NUnit.Framework;
+using Testing.Commons.Globalization;
+
+namespace Testing.Commons.Tests.Globalization
+{
+	[TestFixture]
+	public class CultureReseterTester
+	{
+		[Test]
+		public void Reset_NoChange_ResetToPreviousValues()
+		{
+			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+			CultureInfo uICulture = Thread.CurrentThread.CurrentUICulture;
+
+			using (new CultureReseter())
+			{
+				// do nothing
+			}
+			Assert.That(Thread.CurrentThread.CurrentCulture, Is.EqualTo(culture));
+			Assert.That(Thread.CurrentThread.CurrentUICulture, Is.EqualTo(uICulture));
+		}
+
+		[Test]
+		public void Reset_CultureChange_ResetToPreviousValues()
+		{
+			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+			CultureInfo uICulture = Thread.CurrentThread.CurrentUICulture;
+
+			using (new CultureReseter())
+			{
+				// valid test point if code does not run in Maldives
+				Thread.CurrentThread.CurrentCulture = new CultureInfo("dv-MV");
+				Assert.That(Thread.CurrentThread.CurrentCulture, Is.Not.EqualTo(culture));
+			}
+			Assert.That(Thread.CurrentThread.CurrentCulture, Is.EqualTo(culture));
+			Assert.That(Thread.CurrentThread.CurrentUICulture, Is.EqualTo(uICulture));
+		}
+
+		[Test]
+		public void Reset_CultureUiChange_ResetToPreviousValues()
+		{
+			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+			CultureInfo uICulture = Thread.CurrentThread.CurrentUICulture;
+
+			using (new CultureReseter())
+			{
+				// valid test point if code does not run in Maldives
+				Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("dv");
+				Assert.That(Thread.CurrentThread.CurrentUICulture, Is.Not.EqualTo(uICulture));
+			}
+			Assert.That(Thread.CurrentThread.CurrentCulture, Is.EqualTo(culture));
+			Assert.That(Thread.CurrentThread.CurrentUICulture, Is.EqualTo(uICulture));
+		}
+
+		[Test]
+		public void Reset_BothChange_ResetToPreviousValues()
+		{
+			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+			CultureInfo uICulture = Thread.CurrentThread.CurrentUICulture;
+
+			using (new CultureReseter())
+			{
+				// valid test point if code does not run in Maldives
+				Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("dv");
+				Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("dv-MV");
+				Assert.That(Thread.CurrentThread.CurrentCulture, Is.Not.EqualTo(culture));
+				Assert.That(Thread.CurrentThread.CurrentUICulture, Is.Not.EqualTo(uICulture));
+			}
+			Assert.That(Thread.CurrentThread.CurrentCulture, Is.EqualTo(culture));
+			Assert.That(Thread.CurrentThread.CurrentUICulture, Is.EqualTo(uICulture));
+		}
+
+		[Test]
+		public void Set_BothChange_ResetToPreviousValues()
+		{
+			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+			CultureInfo uICulture = Thread.CurrentThread.CurrentUICulture;
+
+			// valid test point if code does not run in Maldives
+			using (CultureReseter.Set(CultureInfo.GetCultureInfo("dv-MV"), CultureInfo.GetCultureInfo("dv")))
+			{
+				Assert.That(Thread.CurrentThread.CurrentCulture, Is.Not.EqualTo(culture));
+				Assert.That(Thread.CurrentThread.CurrentUICulture, Is.Not.EqualTo(uICulture));
+			}
+			Assert.That(Thread.CurrentThread.CurrentCulture, Is.EqualTo(culture));
+			Assert.That(Thread.CurrentThread.CurrentUICulture, Is.EqualTo(uICulture));
+		}
+
+		[Test]
+		public void Set_Both_CultureAndUiSetToSame()
+		{
+			CultureInfo ci = CultureInfo.GetCultureInfo("dv-MV");
+			using (CultureReseter.Set(ci))
+			{
+				Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(ci));
+				Assert.That(CultureInfo.CurrentUICulture, Is.EqualTo(ci));
+			}
+		}
+
+		[Test]
+		public void Set_NeutralNonNeutralCombinations_NoException()
+		{
+			CultureInfo neutral = CultureInfo.GetCultureInfo("en"), nonNeutral = new CultureInfo("en-GB");
+			Assert.That(() => CultureReseter.Set(nonNeutral, nonNeutral), Throws.Nothing);
+			Assert.That(() => CultureReseter.Set("en-GB", "es-ES"), Throws.Nothing);
+			Assert.That(() => CultureReseter.Set(nonNeutral, neutral), Throws.Nothing);
+			Assert.That(() => CultureReseter.Set("en-US", "es"), Throws.Nothing);
+
+			Assert.That(() => CultureReseter.Set(neutral, neutral), Throws.Nothing);
+			Assert.That(() => CultureReseter.Set(neutral), Throws.Nothing);
+			Assert.That(() => CultureReseter.Set("es", "es"), Throws.Nothing);
+			Assert.That(() => CultureReseter.Set("es"), Throws.Nothing);
+
+			Assert.That(()=> CultureReseter.Set(CultureInfo.InvariantCulture), Throws.Nothing);
+			Assert.That(()=> CultureReseter.Set(CultureInfo.InvariantCulture, CultureInfo.InvariantCulture), Throws.Nothing);
+			
+		}
+
+		[TestCaseSource("cultureCombinations")]
+		public void Set_BothCultures_CanBeNeutralOrNot(CultureInfo culture, CultureInfo uiCulture)
+		{
+			Assert.That(() => CultureReseter.Set(culture, uiCulture), Throws.Nothing);
+		}
+
+		protected IEnumerable<TestCaseData> cultureCombinations
+		{
+			get
+			{
+				CultureInfo neutral = CultureInfo.GetCultureInfo("en"), nonNeutral = new CultureInfo("en-GB");
+				yield return new TestCaseData(nonNeutral, nonNeutral);
+				yield return new TestCaseData(nonNeutral, neutral);
+				yield return new TestCaseData(neutral, neutral).SetDescription("no longer throws");
+			}
+		}
+
+		[TestCaseSource("validCultureNameCombinations")]
+		public void Set_BothCultureNAmes_CultureMustBeNeutralAndUiCultureCanBeNeutralOrNot(string cultureName, string uiCultureName)
+		{
+			Assert.That(() => CultureReseter.Set(cultureName, uiCultureName), Throws.Nothing);
+		}
+
+		protected IEnumerable<TestCaseData> validCultureNameCombinations
+		{
+			get
+			{
+				string neutral = "en", nonNeutral = "en-GB";
+				yield return new TestCaseData(nonNeutral, nonNeutral);
+				yield return new TestCaseData(nonNeutral, neutral);
+			}
+		}
+	}
+}
---
+++ src/Testing.Commons/Globalization/CultureReseter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+
+namespace Testing.Commons.Globalization
+{
+	/// <summary>
+	/// Allows setting and restoring both <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/>.
+	/// </summary>
+	public class CultureReseter : IDisposable
+	{
+		private readonly CultureInfo _threadCulture, _threadUICulture;
+
+		/// <summary>
+		/// Creates an instance of the reseter.
+		/// </summary>
+		/// <remarks>"Freezes" the current information in order to be restored upon disposal.</remarks>
+		public CultureReseter()
+		{
+			backUpThreadCulture(out _threadCulture, out _threadUICulture);
+		}
+
+		private CultureReseter set(CultureInfo threadCulture, CultureInfo threadUICulture)
+		{
+			setThreadCulture(threadCulture, threadUICulture);
+			return this;
+		}
+
+		/// <summary>
+		/// Sets <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/> to the provided cultures.
+		/// </summary>
+		/// <param name="threadCulture">Culture for the current thread.</param>
+		/// <param name="threadUICulture">Culture used by the <see cref="ResourceManager"/></param>
+		/// <returns>An instance of <see cref="CultureReseter"/> with the cultures set and able to restore the previous cultures.</returns>
+		public static CultureReseter Set(CultureInfo threadCulture, CultureInfo threadUICulture)
+		{
+			return new CultureReseter().set(threadCulture, threadUICulture);
+		}
+
+		/// <summary>
+		/// Sets <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/> to the provided cultures.
+		/// </summary>
+		/// <remarks>Instances of <see cref="CultureInfo"/> will be created using <see cref="CultureInfo.GetCultureInfo(string)"/>.</remarks>
+		/// <param name="cultureName">Culture name for the current thread.</param>
+		/// <param name="uiCultureName">Culture name used by the <see cref="ResourceManager"/></param>
+		/// <returns>An instance of <see cref="CultureReseter"/> with the cultures set and able to restore the previous cultures.</returns>
+		public static CultureReseter Set(string cultureName, string uiCultureName)
+		{
+			return Set(CultureInfo.GetCultureInfo(cultureName), CultureInfo.GetCultureInfo(uiCultureName));
+		}
+
+		/// <summary>
+		/// Sets <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/> to the provided culture.
+		/// </summary>
+		/// <param name="bothCultures">Culture for the current thread and used by the <see cref="ResourceManager"/></param>
+		/// <returns>An instance of <see cref="CultureReseter"/> with the cultures set and able to restore the previous cultures.</returns>
+		public static CultureReseter Set(CultureInfo bothCultures)
+		{
+			return new CultureReseter().set(bothCultures, bothCultures);
+		}
+
+		/// <summary>
+		/// Sets <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/> to the provided culture.
+		/// </summary>
+		/// <remarks>Instances of <see cref="CultureInfo"/> will be created using <see cref="CultureInfo.GetCultureInfo(string)"/>.</remarks>
+		/// <param name="bothCultureName">Culture name for the current thread and used by the <see cref="ResourceManager"/></param>
+		/// <returns>An instance of <see cref="CultureReseter"/> with the cultures set and able to restore the previous cultures.</returns>
+		public static CultureReseter Set(string bothCultureName)
+		{
+			return Set(new CultureInfo(bothCultureName));
+		}
+
+		/// <summary>
+		/// Restores <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/> to the previous values.
+		/// </summary>
+		public void Dispose()
+		{
+			setThreadCulture(_threadCulture, _threadUICulture);
+		}
+
+		private static void backUpThreadCulture(out CultureInfo threadCulture, out CultureInfo threadUICulture)
+		{
+			threadCulture = CultureInfo.CurrentCulture;
+			threadUICulture = CultureInfo.CurrentUICulture;
+		}
+
+		private static void setThreadCulture(CultureInfo threadCulture, CultureInfo threadUICulture)
+		{
+			Thread.CurrentThread.CurrentCulture = threadCulture;
+			Thread.CurrentThread.CurrentUICulture = threadUICulture;
+		}
+	}
+}
