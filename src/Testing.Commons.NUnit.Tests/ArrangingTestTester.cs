3c5e6745da3deda49b810ace18a9a5bb31a15d67
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2011-10-03 23:08:51 +0200

    added ArrangingTest

---
+++ src/Testing.Commons.NUnit.Tests/ArrangingTestTester.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Rhino.Mocks.Exceptions;
+using Testing.Commons.NUnit.Tests.Subjects;
+
+namespace Testing.Commons.NUnit.Tests
+{
+	public class ArrangingTestTester : ArrangingTest<string>
+	{
+		private static int COUNTER;
+
+		protected override string initSubject()
+		{
+			COUNTER++;
+			return "subject";
+		}
+
+		[Test]
+		public void Subject_IsInitialized()
+		{
+			Assert.That(Subject, Is.EqualTo("subject"));
+		}
+
+		[Test]
+		public void ArrangingMethod_InvokedBeforeThisTest() { }
+
+		[Test]
+		public void ArrangingMethod_AndBeforeThisTest() { }
+
+		[TestFixtureTearDown]
+		public void AfterAllTestAreRun()
+		{
+			Assert.That(COUNTER, Is.EqualTo(3), "run for every test in the fixture");
+		}
+	}
+
+	public class UsefulWhenDealingWithSubjectsWithMockableDependencies : ArrangingTest<ISubjectWithDependencies>
+	{
+		// dependencies go into fields for test to access them
+		private IDependencyOne _one;
+		private IDependencyTwo _two;
+
+		protected override ISubjectWithDependencies initSubject()
+		{
+			// dependencies are initialized inside the arrange method
+			_one = MockRepository.GenerateStub<IDependencyOne>();
+			_two = MockRepository.GenerateStub<IDependencyTwo>();
+
+			return new SubjectWithDependencies(_one, _two);
+		}
+
+		[Test]
+		public void DoSomethingWithOne_InvokesDependencyOne()
+		{
+			// Act
+			Subject.DoSomethingWithOne();
+
+			// Assert
+			_one.AssertWasCalled(o => o.DoSomething());
+		}
+
+		[Test]
+		public void DoSomethingWithOne_InvokesDependencyTwo()
+		{
+			// Act
+			Subject.DoSomethingWithTwo();
+
+			// Assert
+			_two.AssertWasCalled(o => o.DoSomethingElse());
+		}
+
+		[Test]
+		public void DoSomethingWithBoth_InvokesDependencyTwo()
+		{
+			// Act
+			Subject.DosomethingWithBoth();
+
+			// Assert
+			_one.AssertWasCalled(o => o.DoSomething());
+			_two.AssertWasCalled(o => o.DoSomethingElse());
+		}
+
+		[Test]
+		public void IndividualTests_CanOverrideDependencyBehavior()
+		{
+			// init the subject in a custom method
+			initSubjectWith(MockRepository.GenerateStrictMock<IDependencyTwo>());
+
+			// no expectation on a strict mock makes the mock throw
+
+			Assert.That(() => Subject.DoSomethingWithTwo(), Throws.InstanceOf<ExpectationViolationException>());
+		}
+
+		private void initSubjectWith(IDependencyTwo dependencyTwo)
+		{
+			Subject = new SubjectWithDependencies(_one, dependencyTwo);
+		}
+	}
+}
---
+++ src/Testing.Commons.NUnit/ArrangingTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Testing.Commons.NUnit
+{
+	/// <summary>
+	/// Eases the "arrange" part of AAA tests
+	/// </summary>
+	/// <remarks>Already annotated with the <see cref="TestFixtureAttribute"/> which means that inheritors do not need to specify it again.</remarks>
+	/// <typeparam name="TSubject">Type of the subject (aka. SUT) of the test.</typeparam>
+	[TestFixture]
+	public abstract class ArrangingTest<TSubject>
+	{
+		/// <summary>
+		/// Invokes the "arrange" part of a AAA test.
+		/// </summary>
+		/// <remarks>Annotated with the <see cref="SetUpAttribute"/> which means that it will be run before each test.</remarks>
+		[SetUp]
+		public void Arrange()
+		{
+			Subject = initSubject();
+		}
+
+		/// <summary>
+		/// The System Under Test (SUT)
+		/// </summary>
+		protected TSubject Subject { get; set; }
+
+		/// <summary>
+		/// Implement this method to initialize your System Under Test (SUT).
+		/// </summary>
+		/// <returns>An instance of the SUT.</returns>
+		protected abstract TSubject initSubject();
+	}
+}
