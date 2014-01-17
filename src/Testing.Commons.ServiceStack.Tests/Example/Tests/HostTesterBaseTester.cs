6f5dd97066f8e4c1843db0326ac0652ce728a8df
Daniel González García <danielgonnet@gmail.com>
2014-01-17 11:46:52 +0100

    test customization of the host

---
+++ src/Testing.Commons.ServiceStack.Tests/Example/Tests/HostTesterBaseTester.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework;
+using RestSharp;
+using ServiceStack.WebHost.Endpoints;
+using Testing.Commons.ServiceStack.v3;
+using Testing.Commons.Service_Stack.Tests.Example.Infrastructure.Shared;
+
+namespace Testing.Commons.Service_Stack.Tests.Example.Tests
+{
+	// tests for shared functionality, personalized by SingleHostPerFixture
+	public class HostTesterBaseTester : SingleHostPerFixture
+	{
+		protected override string ServiceName { get { return HostInfo.ServiceName; } }
+
+		protected override IEnumerable<Assembly> AssembliesWithServices { get { return HostInfo.AssembliesWithServices; } }
+
+		protected override void Boootstrap(IAppHost arg)
+		{
+			// normally inside a shared IAppBootstrapper of some kind
+			arg.Config.PostExecuteServiceFilter = (dto, request, response) =>
+			{
+				if (response.StatusCode == 200) response.StatusDescription = "SUPER GREEN";
+			};
+		}
+
+		[Test]
+		public void AllowsSharingConfiguration_BetweenImplementation_AndTests()
+		{
+			string text = "something";
+			var client = new RestClient(BaseUrl.ToString());
+			var request = new RestRequest("/echo", Method.GET);
+			request.AddParameter("Text", text);
+
+			var response = client.Execute(request);
+
+			Assert.That(response.StatusDescription, Is.EqualTo("SUPER GREEN"));
+		}		
+	}
+}
