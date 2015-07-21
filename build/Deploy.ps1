be7d44aa9258b51687e171fefaa9b951f35b7180
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-07-21 13:29:52 +0200

    add local deployment script

---
+++ build/Deploy.ps1
@@ -0,0 +1,42 @@
+$script_directory = Split-Path -parent $PSCommandPath
+$base_dir = Resolve-Path $script_directory\..
+
+Remove-Module [N]Moneys
+Import-Module "$script_directory\Testing.Commons.psm1" -DisableNameChecking
+
+function Push-Package($package)
+{
+	$nuget = Join-Path $base_dir tools\nuget\nuget.exe
+	$release_dir = Join-Path $base_dir release
+
+	$pkg = Get-ChildItem -File "$release_dir\$package*.nupkg" |
+		? { $_.Name -match "$package\.(\d(?:\.\d){3})" }  | 
+		% { 
+			Write-Host "& $nuget push $_.FullName"
+			Throw-If-Error
+		}
+}
+
+$title = "Push Packages"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+    "Push the package to nuget.org."
+
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+    "Does not push the package to nuget.org."
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+('Testing.Commons', 'Testing.Commons.NUnit', 'Testing.Commons.ServiceStack') |
+	% {
+	$package = $_
+	$message = "Do you want to push $package.*.nupkg to NuGet?"
+	$result = $host.ui.PromptForChoice($title, $message, $options, 1)
+	switch ($result)
+	{
+		0 {
+			"Pushing $package package..."
+			Push-Package $package
+		}
+		1 {"No-op."}
+	}
+}
