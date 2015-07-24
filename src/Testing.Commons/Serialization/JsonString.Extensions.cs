8d1a24c29b1dcac3ff5cf63e819768a4fb9502c3
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-07-24 14:12:14 +0200

    added JsonString and .Jsonify() extensions

---
+++ src/Testing.Commons/Serialization/JsonString.Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Testing.Commons.Serialization
+{
+	public static class JsonStringExtensions
+	{
+		public static string Jsonify(this string json)
+		{
+			return JsonString.jsonify(json);
+		}
+	}
+}
---
+++ src/Testing.Commons/Serialization/JsonString.cs
@@ -0,0 +1,26 @@
+﻿namespace Testing.Commons.Serialization
+{
+	public class JsonString
+	{
+		private readonly string _json;
+		public JsonString(string json)
+		{
+			_json = jsonify(json);
+		}
+
+		internal static string jsonify(string s)
+		{
+			return s == null ? null : s.Replace("'", "\"");
+		}
+
+		public override string ToString()
+		{
+			return _json;
+		}
+
+		public static implicit operator string(JsonString instance)
+		{
+			return instance._json;
+		}
+	}
+}
