850b768df99210d945c24f22a893634813989db2
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-06-08 16:37:48 +0200

    confirm the need for EnumerableCountConstraint

---
+++ src/Testing.Commons.NUnit.Tests/Constraints/EnumerableCountConstraintTester.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Testing.Commons.NUnit.Tests.Constraints
+{
+	[TestFixture]
+	public class EnumerableCountConstraintTester
+	{
+		#region Exploratory
+
+		[Test]
+		public void Array_MustUseLength()
+		{
+			Assert.That(new[] { 1, 2, 3 }, Has.Length.EqualTo(3));
+		}
+
+		[Test]
+		public void List_MustUseCount()
+		{
+			Assert.That(new List<int> { 1, 2, 3 }, Has.Count.EqualTo(3));
+		}
+
+		[Test]
+		public void Enumerable_FromArray_MustUseLength()
+		{
+			IEnumerable<int> e = new[] { 1, 2, 3 };
+			Assert.That(e, Has.Length.EqualTo(3));
+		}
+
+		[Test]
+		public void Enumerable_FromList_MustUseCount()
+		{
+			IEnumerable<int> e = new List<int> { 1, 2, 3 };
+			Assert.That(e, Has.Count.EqualTo(3));
+		}
+
+		[Test]
+		public void Enumereble_FromLinq_CannotUseLengthOrCount()
+		{
+			IEnumerable<int> e = new[] { 1, 2, 3 }.Where(i => i <= 2);
+
+			TestDelegate assertion = () => Assert.That(e, Has.Length.EqualTo(2));
+			Assert.That(assertion, Throws.ArgumentException.With.Message.Contain("Length"));
+
+			assertion = () => Assert.That(e, Has.Count.EqualTo(2));
+			Assert.That(assertion, Throws.ArgumentException.With.Message.Contain("Count"));
+		}
+
+		#endregion
+	}
+}
