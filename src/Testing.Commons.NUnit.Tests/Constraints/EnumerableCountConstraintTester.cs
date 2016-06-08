8f58fbe2eb7fe04ee9f0085d751f83c5cecd2ac8
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-06-08 16:40:30 +0200

    skeleton for EnumerableCountConstraint

--- src/Testing.Commons.NUnit.Tests/Constraints/EnumerableCountConstraintTester.cs
+++ src/Testing.Commons.NUnit.Tests/Constraints/EnumerableCountConstraintTester.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace Testing.Commons.NUnit.Tests.Constraints
 {
@@ -48,5 +50,48 @@ namespace Testing.Commons.NUnit.Tests.Constraints
 		}
 
 		#endregion
+
+		[Test]
+		public void CanBeNewedUp()
+		{
+			IEnumerable e = new[] { 1, 2, 3 }.Where(i => i <= 2);
+			Assert.That(e, new EnumerableCountConstraint(Is.LessThan(3)));
+		}
+
+		[Test]
+		public void CanBeCreatedWithExtension()
+		{
+			IEnumerable e = new[] { 1, 2, 3 }.Where(i => i <= 2);
+			Assert.That(e, Must.Have.Count(Is.LessThan(3)));
+		}
+	}
+
+	public class EnumerableCountConstraint : Constraint
+	{
+		private readonly Constraint _constraint;
+
+		public EnumerableCountConstraint(Constraint constraint)
+		{
+			_constraint = constraint;
+		}
+
+		public override ConstraintResult ApplyTo<TActual>(TActual actual)
+		{
+			return new ConstraintResult(this, actual, true);
+		}
+	}
+
+	public static partial class MustExtensions
+	{
+		/// <summary>
+		/// Builds an instance of <see cref="EnumerableCountConstraint"/> that allows asserting on the number of elements of any instance of <see cref="System.Collections.IEnumerable"/>.
+		/// </summary>
+		/// <param name="entry">Extension entry point.</param>
+		/// <param name="countConstraint">The constraint to be applied to the element count.</param>
+		/// <returns>Instance built.</returns>
+		public static Constraint Count(this Must.HaveEntryPoint entry, Constraint countConstraint)
+		{
+			return new EnumerableCountConstraint(countConstraint);
+		}
 	}
 }
