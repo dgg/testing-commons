dcbb5ed5abd6029b53c3b67b5c5da03a867e2821
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-06-08 16:13:14 +0200

    test first item does not match constraint

--- src/Testing.Commons.NUnit.Tests/Constraints/ConstrainedEnumerableTester.cs
+++ src/Testing.Commons.NUnit.Tests/Constraints/ConstrainedEnumerableTester.cs
@@ -22,11 +22,19 @@ namespace Testing.Commons.NUnit.Tests.Constraints
 		}
 
 		[Test]
-		public void applyTo_SubjectHasMoreItemsThanConstraintsProvided_Failure()
+		public void ApplyTo_SubjectHasMoreItemsThanConstraintsProvided_Failure()
 		{
 			var subject = new ConstrainedEnumerable(Is.EqualTo(1));
 
 			Assert.That(matches(subject, new[] { 1, 2 }), Is.False);
+		}
+
+		[Test]
+		public void Apply_FirstItemDoesNotMatchConstraint_False()
+		{
+			var subject = new ConstrainedEnumerable(Is.GreaterThan(0), Is.EqualTo(2));
+
+			Assert.That(matches(subject, new[] { -1, 2 }), Is.False);
 		}
 
 		#endregion
@@ -51,6 +59,16 @@ namespace Testing.Commons.NUnit.Tests.Constraints
 			Assert.That(getMessage(subject, new[] { 1, 2 }),
 				Does.Contain(TextMessageWriter.Pfx_Expected + 2).And
 				.Contain(TextMessageWriter.Pfx_Actual + 1));
+		}
+
+		[Test]
+		public void WriteMessageTo_FirstItemDoesNotMatchConstraint_DescriptionContainsIndexOfFailingItem_ExpectedIsValueOfFailingConstraint_ActualIsValueOfOffendingItem()
+		{
+			var subject = new ConstrainedEnumerable(Is.GreaterThan(0), Is.EqualTo(2));
+
+			Assert.That(getMessage(subject, new[] { -1, 2 }), Does.Contain("# 0").And
+				.Contain(TextMessageWriter.Pfx_Expected + "greater than 0").And
+				.Contain(TextMessageWriter.Pfx_Actual + "-1"));
 		}
 
 		#endregion
@@ -91,14 +109,28 @@ namespace Testing.Commons.NUnit.Tests.Constraints
 		public override ConstraintResult ApplyTo<TActual>(TActual actual)
 		{
 			// it is ok to iterate the collection as most of the times it will be a small controlled collection
-			var _collection = (actual as IEnumerable).Cast<object>().ToArray();
-			return sameNumberOfElementsAndConstraints(_collection, _constraints.Length);
+			var collection = (actual as IEnumerable).Cast<object>().ToArray();
+
+			ConstraintResult result = sameNumberOfElementsAndConstraints(collection, _constraints.Length);
+			if (result.IsSuccess)
+			{
+				for (int i = 0; i < collection.Length && result.IsSuccess; i++)
+				{
+					_inner = new IndexedConstraint(collection, i, _constraints[i]);
+					result = _inner.ApplyTo(collection.GetValue(i));
+				}	
+			}
+
+			return result;
 		}
+
+		public override string Description => _inner.Description;
 
 		private ConstraintResult sameNumberOfElementsAndConstraints(Array current, int numberOfConstraints)
 		{
 			_inner = new MatchingLength(current, current.Length);
-			return _inner.ApplyTo(numberOfConstraints);
+			var result = _inner.ApplyTo(numberOfConstraints);
+			return result;
 		}
 
 		/// <summary>
@@ -116,14 +148,14 @@ namespace Testing.Commons.NUnit.Tests.Constraints
 
 			public override ConstraintResult ApplyTo<TActual>(TActual actual)
 			{
-				return new MatchingLengthResult(this, _items, base.ApplyTo(actual));
+				return new MatchingLengthResult(_items, this, base.ApplyTo(actual));
 			}
 
 			class MatchingLengthResult : ConstraintResult
 			{
 				private readonly Array _items;
 
-				public MatchingLengthResult(IConstraint constraint, Array items, ConstraintResult result) : base(constraint, result.ActualValue, result.IsSuccess)
+				public MatchingLengthResult(Array items, IConstraint constraint, ConstraintResult result) : base(constraint, result.ActualValue, result.IsSuccess)
 				{
 					_items = items;
 				}
@@ -131,6 +163,55 @@ namespace Testing.Commons.NUnit.Tests.Constraints
 				public override void WriteMessageTo(MessageWriter writer)
 				{
 					writer.WriteMessageLine(0, "{0} has a different number of items than constraints are provided.", getCollectionValue(writer));
+					base.WriteMessageTo(writer);
+				}
+
+				private string getCollectionValue(MessageWriter writer)
+				{
+					writer.WriteActualValue(_items);
+					var sb = writer.GetStringBuilder();
+					string items = sb.ToString();
+					sb.Remove(0, sb.Length);
+					return items;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Encapsulates a contraint applied to an enumerable element to enhance the feedback in case of failure by providing the index of the element that provoked the failure.
+		/// </summary>
+		class IndexedConstraint : Constraint
+		{
+			private readonly Array _items;
+			private readonly int _index;
+			private readonly IConstraint _constraint;
+			public IndexedConstraint(Array items, int index, IResolveConstraint constraint)
+			{
+				_items = items;
+				_index = index;
+				_constraint = constraint.Resolve();
+			}
+
+			public override ConstraintResult ApplyTo<TActual>(TActual actual)
+			{
+				return new IndexedResult(_items, _index, _constraint, _constraint.ApplyTo(actual));
+			}
+
+			class IndexedResult : ConstraintResult
+			{
+				private readonly Array _items;
+				private readonly int _index;
+
+				public IndexedResult(Array items, int index, IConstraint constraint, ConstraintResult result) : base(constraint, result.ActualValue, result.IsSuccess)
+				{
+					_items = items;
+					_index = index;
+				}
+
+				public override void WriteMessageTo(MessageWriter writer)
+				{
+					string collectionValue = getCollectionValue(writer);
+					writer.WriteMessageLine(0, "Element # {0} from {1} failed to satisfy constraint.", _index, collectionValue);
 					base.WriteMessageTo(writer);
 				}
 
