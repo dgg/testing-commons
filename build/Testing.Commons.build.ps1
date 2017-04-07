8cd32d253902e5c9b875647d1417b83d1442dc20
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-04-07 14:15:46 +0200

    upgrade psake and use alternative method for finding msbuild

--- build/Testing.Commons.build.ps1
+++ build/Testing.Commons.build.ps1
@@ -1,4 +1,4 @@
-Framework 4.5.2
+Framework "4.6"
 
 properties {
 	$configuration = 'Debug'
@@ -8,22 +8,30 @@ properties {
 
 task default -depends Clean, Compile, Test, CopyArtifacts, BuildArtifacts
 
-task Clean {
-	exec { msbuild "$base_dir\Testing.Commons.sln" /t:clean /p:configuration=$configuration /m }
+task Clean -depends importModules {
+	$msbuild = find_msbuild
+
+	exec { & $msbuild "$base_dir\Testing.Commons.sln" /t:clean /p:configuration=$configuration /m }
 	Remove-Item $release_dir -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 }
 
-task Compile {
-	exec { msbuild "$base_dir\Testing.Commons.sln" /p:configuration=$configuration /m }
+task Compile -depends importModules {
+	$msbuild = find_msbuild
+
+	exec { & $msbuild "$base_dir\Testing.Commons.sln" /p:configuration=$configuration /m }
 }
 
-task ensureRelease -depends importModule {
+task ensureRelease -depends importModules {
 	Ensure-Release-Folders $base_dir
 }
 
-task importModule {
+task importModules {
 	Remove-Module [T]esting.Commons
 	Import-Module "$base_dir\build\Testing.Commons.psm1" -DisableNameChecking
+
+	Remove-Module [V]SSetup
+	$vssetup_dir = find_versioned_dir -base $base_dir\tools -beginning 'VSSetup'
+	Import-Module  $vssetup_dir\VSSetup.psd1 -DisableNameChecking
 }
 
 task Test -depends ensureRelease {
@@ -54,10 +62,7 @@ function get-test-assembly-name($base, $config, $name)
 }
 
 function run_tests($base, $release, $test_assemblies){
-	$console_dir = Get-ChildItem $base\tools\* -Directory | where {$_.Name.StartsWith('NUnit.ConsoleRunner')}
-    # get first directory
-    $console_dir = $console_dir[0]
-
+	$console_dir = find_versioned_dir -base $base\tools -beginning 'NUnit.ConsoleRunner'
 	$nunit_console = Join-Path $console_dir tools\nunit3-console.exe
 	exec { & $nunit_console $test_assemblies --result:"$release\TestResult.xml" --noheader  }
 }
@@ -69,4 +74,17 @@ function report-on-test-results($base, $release)
 	$output_html = Join-Path $release TestResult.html
 	
 	exec { & $nunit_orange $input_xml $output_html }
+}
+
+function find_versioned_dir($base, $beginning)
+{
+	$dir = Get-ChildItem $base -Directory | where { $_.Name.StartsWith($beginning) }
+	return Join-Path $base $dir[0]
+}
+
+function find_msbuild()
+{
+	$vs_dir = Get-VSSetupInstance | Select-Object -ExpandProperty InstallationPath
+	$msbuild = Join-Path $vs_dir MSBuild\15.0\Bin\MSBuild.exe
+	return $msbuild;
 }
