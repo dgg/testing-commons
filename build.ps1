4790945342f9be213641284e936603a4fce5f750
Daniel González García <danielgonnet@gmail.com>
2023-08-17 08:16:53 +0200

    almost correct psake build

--- build.ps1
+++ build.ps1
@@ -2,81 +2,20 @@
 Param(
 	[ValidateSet('Debug', 'Release')]
 	[string]$configuration = 'Release',
+	[ValidateSet('q', 'm', 'n', 'd')]
 	[string]$verbosity = 'q',
-	[string]$task = "Build"
+	[string[]]$task = @()
 )
 
+$base_dir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-function Clean() {
-	& dotnet clean -c $configuration -v $verbosity --nologo
-	$release_dir = Join-Path $base_dir release
-	Remove-Item $release_dir -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
-}
+$tools_pattern = Join-Path $base_dir tools *
+$psake_path = Get-ChildItem -Directory -Path $tools_pattern |
+Where-Object { $_.Name.StartsWith('psake') }
+# first psake directory
+$psake_script = Join-Path $psake_path[0] psake.ps1
 
-function Restore() {
-	& dotnet restore -v $verbosity
-}
-
-function Compile() {
-	& dotnet build --no-restore -c $configuration -v $verbosity --nologo
-}
-
-function Test() {
-	$release_dir = Join-Path $base_dir release
-
-	$trx = Join-Path TestResults Testing.Commons.Tests.trx
-	$html = Join-Path TestResults Testing.Commons.Tests.html
-
-	& dotnet test --no-build -c $configuration --nologo -v $verbosity `
-		--results-directory $release_dir `
-		-l:"console;verbosity=minimal;NoSummary=true" `
-		-l:"trx;LogFileName=$trx" `
-		-l:"html;LogFileName=$html" `
-		-- NUnit.TestOutputXml=TestResults NUnit.OutputXmlFolderMode=RelativeToResultDirectory
-
-	# TODO: generate extra HTML reports
-}
-
-function Pack() {
-	& dotnet pack --no-build -c $configuration --nologo -v $verbosity
-}
-
-function CopyArtifacts() {
-	$release_dir = Join-Path $base_dir release
-
-	Join-Path $base_dir src Testing.Commons bin $configuration |
-	Get-ChildItem -Recurse |
-	Where-Object { $_.Name -match '.dll|.xml|.pdb|.nupkg' } |
-	Copy-Item -Destination $release_dir
-}
-
-function Publish() {
-	Join-Path $base_dir src Testing.Commons bin $configuration |
-	Get-ChildItem -Recurse |
-	Where-Object { $_.Name -match '.nupkg' } |
-	push
-}
-
-function push {
-	param (
-		[Parameter(ValueFromPipeline = $true)][System.IO.FileInfo]$nupkg
-	)
-	PROCESS {
-		& dotnet nuget push -s nuget.org $nupkg.FullName
-	}
-}
-
-function Build () {
-	& Clean
-	& Restore
-	& Compile
-	& Test
-	& Pack
-	& CopyArtifacts
-}
-
-$script_directory = Split-Path -parent $PSCommandPath
-$base_dir = resolve-path $script_directory
-
-& $task
-exit $LASTEXITCODE;
+$build_file = Join-Path $base_dir build Testing.Commons.build.ps1
+& $psake_script -nologo `
+	-buildFile $build_file -taskList $task `
+	-properties @{configuration = $configuration; verbosity = $verbosity }
