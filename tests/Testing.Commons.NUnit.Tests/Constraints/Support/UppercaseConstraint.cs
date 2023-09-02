00945221ee9cd3ecb96d096dafaf9dd7564430e6
Daniel González García <danielgonnet@gmail.com>
2023-09-02 15:59:00 +0200

    added DelegatingContraint and fix folder name

---
+++ tests/Testing.Commons.NUnit.Tests/Constraints/Support/UppercaseConstraint.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace Testing.Commons.NUnit.Tests.Constraints.Support;
+internal class UppercaseConstraint : Constraint
+{
+	public override ConstraintResult ApplyTo<TActual>(TActual actual)
+	{
+		return new ConstraintResult(this, actual, match(actual));
+	}
+
+	private bool match(object current)
+	{
+		var c = (char)current;
+		return char.IsUpper(c);
+	}
+
+	public override string Description => "An uppercase character";
+}
