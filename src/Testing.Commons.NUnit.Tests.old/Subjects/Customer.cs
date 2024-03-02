fd431552e8232089170993f887eefb867ff97231
Daniel González García <danielgonnet@gmail.com>
2024-03-02 09:33:44 +0100

    remove old tests

--- src/Testing.Commons.NUnit.Tests.old/Subjects/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Testing.Commons.NUnit.Tests.Subjects
-{
-	class Customer
-	{
-		public string Name { get; set; }
-		public string PhoneNumber { get; set; }
-		public Address Address { get; set; }
-	}
-}
--- src/Testing.Commons.NUnit.Tests.old/Subjects/FlatCustomer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Testing.Commons.NUnit.Tests.Subjects
-{
-	class FlatCustomer
-	{
-		public string Name { get; set; }
-		public string PhoneNumber { get; set; }
-	}
-}
